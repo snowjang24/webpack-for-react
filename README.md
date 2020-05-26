75f7f7cf3fb486d016a0f39146379d6be21f4099
snowjang24 <asdka4618@gmail.com>
2020-05-26 23:22:48 +0900

    feat: webpack for react 03

--- README.md
+++ README.md
@@ -5,13 +5,14 @@
 - [x] 초기 디펜던시 설정
 - [x] Webpack 설정
 - [x] React 애플리케이션 만들기(기본)
+- [x] React Hot Module Replacement
+- [x] Code Splitting
 
 ##  예제 및 글
 
 - [📄 React를 위한 Webpack 배우기 - 01](https://jsnow.netlify.app/posts/webpack-for-react-01)
-
 - [🗂 Example - 01](https://github.com/snowjang24/webpack-for-react/tree/c4668443c27c260fa7744950663b53dfa3b0727a)
-
 - [📄 React를 위한 Webpack 배우기 - 02](https://jsnow.netlify.app/posts/webpack-for-react-02)
-
-- [🗂 Example - 02]()
+- [🗂 Example - 02](https://github.com/snowjang24/webpack-for-react/tree/916fee6f6a48a5ddaf6642226dfcd1609ca7c3d1)
+- [📄 React를 위한 Webpack 배우기 - 03](https://jsnow.netlify.app/posts/webpack-for-react-03)
+- [🗂 Example - 03]()
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const port = process.env.PORT || 3000;
-
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    vendor: ["semantic-ui-react"],
+    app: "./src/index.js",
+  },
   output: {
-    filename: "bundle.[hash].js",
+    filename: "[name].[hash].js",
+  },
+  resolve: {
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
   },
   devtool: "inline-source-map",
   module: {
     rules: [
-      // First Rule
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
-      // Second Rule
       {
         test: /\.css$/,
         use: [
@@ -36,7 +41,26 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+        vendor: {
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true,
+        },
+      },
+    },
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: "public/index.html",
       favicon: "public/favicon.ico",
@@ -47,5 +71,6 @@ module.exports = {
     port: port,
     historyApiFallback: true,
     open: true,
+    hot: true,
   },
 };
